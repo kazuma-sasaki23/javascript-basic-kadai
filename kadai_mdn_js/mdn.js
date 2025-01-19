b0cd8074641181b5af1d11764f9c2048fe141270
kazuma-sasaki23 <kusuoman.07@gmail.com>
2025-01-19 22:59:37 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getUTCFullYear();
+const month = today.getUTCMonth() + 1;
+const dayOfMonth = today.getUTCDate();
+
+console.log(year + '年' + month + '月' + dayOfMonth + '日');
